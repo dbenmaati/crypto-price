31b944da57a72897e4d5c6291fdaf029407eca4f
Dhia Ben maati <d.benmaati@gmail.com>
2024-06-05 01:37:20 +0100

    Update migrations

--- database/migrations/2024_06_04_004758_create_coins_table.php
+++ database/migrations/2024_06_04_004758_create_coins_table.php
@@ -14,21 +14,22 @@ return new class extends Migration
         Schema::create('coins', function (Blueprint $table) {
             $table->id();
             $table->string('slug',100);
+            $table->integer('rank'); 
             $table->string('name',100);
             $table->string('symbol',100);
             $table->string('logo',100);
-            $table->text('description');
+            $table->text('description')->nullable();
 
-            $table->string('website',100);
-            $table->string('whitepaper',100);
+            $table->string('website',100)->nullable();
+            $table->string('whitepaper',100)->nullable();
 
-            $table->string('twitter',100);
-            $table->string('telegram',100);
-            $table->string('discord',100);
-            $table->string('explored',100);
+            $table->string('twitter',100)->nullable();
+            $table->string('telegram',100)->nullable();
+            $table->string('discord',100)->nullable();
+            $table->string('explorer',100)->nullable();
 
-
-            $table->timestamps();
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
--- database/migrations/2024_06_04_005001_create_exchanges_table.php
+++ database/migrations/2024_06_04_005001_create_exchanges_table.php
@@ -14,11 +14,14 @@ return new class extends Migration
         Schema::create('exchanges', function (Blueprint $table) {
             $table->id();
             $table->string('slug',100);
+            $table->integer('rank'); 
             $table->string('name',100);
             $table->string('logo',100);
-            $table->text('description');
+            $table->text('description')->nullable();
             $table->string('website',100);
-            $table->timestamps();
+            
+            $table->timestamp('created_at')->default(DB::raw('CURRENT_TIMESTAMP'));
+            $table->timestamp('updated_at')->default(DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
         });
     }
 
